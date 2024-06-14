6550b4651031aff70803b1458057cea17e7c4828
babireski <elian.babireski@gmail.com>
2024-06-14 18:27:40 -0300

    Typescheme type added

--- src/Type.hs
+++ src/Type.hs
@@ -1,9 +1,14 @@
 module Type where
 
 data Type = Variable String | Type :→: Type | Type :+: Type | Type :×: Type
+data Typescheme = Type Type | Universal String Typescheme
 
 instance Show Type where
     show (Variable α) = α
     show (τ₁ :→: τ₂)  = show τ₁ ++ " → " ++ show τ₂
     show (τ₁ :+: τ₂)  = show τ₁ ++ " + " ++ show τ₂
     show (τ₁ :×: τ₂)  = show τ₁ ++ " × " ++ show τ₂
+
+instance Show Typescheme where
+    show (Type τ) = show τ
+    show (Universal α σ) = "∀" ++ α ++ "." ++ show σ
