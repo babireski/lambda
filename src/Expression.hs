487d3a5ae304259b3acdb86ac01dcd65668f75ca
babireski <elian.babireski@gmail.com>
2024-06-17 13:15:18 -0300

    Typing added

--- src/Expression.hs
+++ src/Expression.hs
@@ -3,6 +3,7 @@ module Expression where
 data Expression = Abstraction String Expression | Application Expression Expression | Variable String
 
 instance Show Expression where
+    show :: Expression -> String
     show (Variable x) = x
     show (Abstraction x e) = "λ" ++ x ++ "." ++ show e
     show (Application e₁ e₂) = show e₁ ++ show e₂
--- src/Type.hs
+++ src/Type.hs
@@ -4,11 +4,13 @@ data Type = Variable String | Type :→: Type | Type :+: Type | Type :×: Type
 data Typescheme = Type Type | Universal String Typescheme
 
 instance Show Type where
+    show :: Type -> String
     show (Variable α) = α
     show (τ₁ :→: τ₂)  = show τ₁ ++ " → " ++ show τ₂
     show (τ₁ :+: τ₂)  = show τ₁ ++ " + " ++ show τ₂
     show (τ₁ :×: τ₂)  = show τ₁ ++ " × " ++ show τ₂
 
 instance Show Typescheme where
+    show :: Typescheme -> String
     show (Type τ) = show τ
     show (Universal α σ) = "∀" ++ α ++ "." ++ show σ
